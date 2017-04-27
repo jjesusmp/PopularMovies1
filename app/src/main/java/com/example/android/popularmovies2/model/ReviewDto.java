7c7a18f4bee5da1579a80dd01069ae0368dca39c
jjesusmp <jjesusmp@gmail.com>
2017-04-27 02:00:01 +0200

    first commit

---
+++ app/src/main/java/com/example/android/popularmovies2/model/ReviewDto.java
@@ -0,0 +1,8 @@
+package com.example.android.popularmovies2.model;
+
+/**
+ * Created by jjesusmp on 26/04/2017.
+ */
+
+public class ReviewDto {
+}
---
+++ app/src/main/java/com/example/android/popularmovies2/model/TrailerDto.java
@@ -0,0 +1,9 @@
+package com.example.android.popularmovies2.model;
+
+/**
+ * Created by jjesusmp on 26/04/2017.
+ */
+
+public class VideoDto {
+
+}
